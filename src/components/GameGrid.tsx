7cde677539706a272238b402992d6aa82f58f4db
Abbas <abbas@iamroff.com>
2024-01-22 15:51:57 +0400

    Refactor: Game grid error line

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,27 +14,26 @@ const GameGrid = ({ gameQuery }: Props) => {
     const { data, error, isLoading } = useGames(gameQuery);
     const sekeletons = [1, 2, 3, 4, 5, 6];
 
+    if (error) return <Text>{error}</Text>;
+
     return (
-        <>
-            {error && <Text>{error}</Text>}
-            <SimpleGrid
-                columns={{ sm: 1, md: 2, lg: 4, "2xl": 7 }}
-                spacing={6}
-                padding="10px"
-            >
-                {isLoading &&
-                    sekeletons.map((skeleton) => (
-                        <GameCardContainer key={skeleton}>
-                            <GameCardSkeleton></GameCardSkeleton>
-                        </GameCardContainer>
-                    ))}
-                {data.map((game) => (
-                    <GameCardContainer key={game.id}>
-                        <GameCard game={game} />
+        <SimpleGrid
+            columns={{ sm: 1, md: 2, lg: 4, "2xl": 7 }}
+            spacing={6}
+            padding="10px"
+        >
+            {isLoading &&
+                sekeletons.map((skeleton) => (
+                    <GameCardContainer key={skeleton}>
+                        <GameCardSkeleton></GameCardSkeleton>
                     </GameCardContainer>
                 ))}
-            </SimpleGrid>
-        </>
+            {data.map((game) => (
+                <GameCardContainer key={game.id}>
+                    <GameCard game={game} />
+                </GameCardContainer>
+            ))}
+        </SimpleGrid>
     );
 };
 
